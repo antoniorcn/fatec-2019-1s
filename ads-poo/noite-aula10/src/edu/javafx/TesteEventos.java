fc59d221d369659f55c0a8ced9c2c40236b52daf
Antonio Rodrigues <antonio@moonsoft.com.br>
2019-04-12 22:09:04 -0300

    Aula poo noite-10 parte 2

--- ads-poo/noite-aula10/src/edu/javafx/TesteEventos.java
+++ ads-poo/noite-aula10/src/edu/javafx/TesteEventos.java
@@ -7,23 +7,29 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-
-class Manipulador implements EventHandler<MouseEvent> {
-	public void handle(MouseEvent e) {
-		String nomeEvento = e.getEventType().getName();
-		String botao = e.getButton().name();
-		String texto = String.format("%s - (%f, %f) - %s", 
-				nomeEvento, e.getX(), e.getY(), botao );
-		System.out.println(texto);
-	} 
-}
 public class TesteEventos extends Application {
+	private Label lblOla;
+	private Pane root;
+	class Manipulador implements EventHandler<MouseEvent> {
+		public void handle(MouseEvent e) {
+			String nomeEvento = e.getEventType().getName();
+			String botao = e.getButton().name();
+			String texto = String.format("%s - (%f, %f) - %s", 
+					nomeEvento, e.getX(), e.getY(), botao );
+			lblOla.setText(texto);
+			lblOla.relocate(e.getX(), e.getY());
+			Rectangle r = new Rectangle(e.getX(), e.getY(), 5, 5);
+			root.getChildren().add(r);
+			
+		} 
+	}
 	public void start(Stage stage) throws Exception {	
 		Manipulador man = new Manipulador();
-		Pane root = new Pane();
+		root = new Pane();
 		Scene scene = new Scene(root, 640, 480);
-		Label lblOla = new Label("Ola mundo");
+		lblOla = new Label("Ola mundo");
 		lblOla.relocate(300, 220);
 		stage.addEventFilter(MouseEvent.ANY, man);
 		root.getChildren().add(lblOla);
