72ade6b746d0c55f70acc98fa65b1d8a6618fed3
Antonio Rodrigues <antonio@moonsoft.com.br>
2019-04-15 08:29:36 -0300

    idw manha aula 8 parte 1

---
+++ djd-idw/manha-aula8/bials.js
@@ -0,0 +1,35 @@
+// Executado no momento de carregamento do HTML
+var canvas;
+var ctx;
+console.log("Javascript bial.js carregado");
+var cenario1 = [   
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
+function inicializa(e) { 
+    // Executado quando o HTML foi totalmente carregado
+    console.log("Conteudo do documento HTML foi carregado");
+    canvas = document.getElementById("game");
+    console.log("Canvas: ");
+    console.log(canvas);
+}
+function executa(e) { 
+    // Executado quando todos os documentos imagens, css, etc forem carregados
+    console.log("Todos os documentos imagens, sons, etc, foram carregados");
+}
+document.addEventListener("DOMContentLoaded", inicializa);
+window.addEventListener("load", executa);
