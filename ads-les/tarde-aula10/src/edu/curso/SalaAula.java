aebd49d809eba4b05ee73125a66420ac249dc71e
Antonio Rodrigues <antonio@moonsoft.com.br>
2019-04-17 17:39:58 -0300

    LES Tarde aula 10 parte 2

---
+++ ads-les/tarde-aula10/src/edu/curso/SalaAula.java
@@ -0,0 +1,55 @@
+package edu.curso;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+@Entity
+public class SalaAula implements Serializable {
+	private static final long serialVersionUID = 2538846510679226307L;
+	private long id;
+	private String numero;
+	private int capacidade;
+	private String disciplina;
+	private List<Aluno> alunos;
+	
+	@Id
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	public int getCapacidade() {
+		return capacidade;
+	}
+	public void setCapacidade(int capacidade) {
+		this.capacidade = capacidade;
+	}
+	
+	public String getDisciplina() {
+		return disciplina;
+	}
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
+	}
+	
+	@OneToMany(targetEntity = Aluno.class)
+	public List<Aluno> getAlunos() {
+		return alunos;
+	}
+	public void setAlunos(List<Aluno> alunos) {
+		this.alunos = alunos;
+	}
+}
--- ads-les/tarde-aula10/src/edu/curso/TesteJPA.java
+++ ads-les/tarde-aula10/src/edu/curso/TesteJPA.java
@@ -1,23 +1,58 @@
 package edu.curso;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 public class TesteJPA {
-	public static void main(String[] args) {
+	public static void criarInstancia(EntityManagerFactory emf) { 
 		Aluno a1 = new Aluno();
 		a1.setId(1);
 		a1.setNascimento(new Date());
 		a1.setNome("Antonio Carvalho");
 		a1.setRa("000001");
-		EntityManagerFactory emf = 
-				Persistence.createEntityManagerFactory("ALUNOS");
+		
+		Aluno a2 = new Aluno();
+		a2.setId(2);
+		a2.setNascimento(new Date());
+		a2.setNome("Andre");
+		a2.setRa("000002");
+		
+		SalaAula s1 = new SalaAula();
+		s1.setId(1);
+		s1.setNumero("102");
+		s1.setDisciplina("Laboratorio de Engenharia de SW");
+		s1.setCapacidade(40);
+		List<Aluno> alunos = new ArrayList<>();
+		alunos.add(a1);
+		alunos.add(a2);
+		s1.setAlunos(alunos);
+		
+		
 		EntityManager em = emf.createEntityManager();
 		em.getTransaction().begin();
 		em.persist(a1);
+		em.persist(a2);
+		em.persist(s1);
 		em.getTransaction().commit();
 		em.close();
+	}
+	
+	public static void atualizar(EntityManagerFactory emf) { 
+		EntityManager em = emf.createEntityManager();
+		Aluno a = em.find(Aluno.class, 1l);
+		em.getTransaction().begin();
+		a.setRa("111111");
+		em.getTransaction().commit();
+		em.close();
+	}
+	
+	public static void main(String[] args) {
+		EntityManagerFactory emf = 
+				Persistence.createEntityManagerFactory("ALUNOS");
+		criarInstancia(emf);
 		emf.close();
 	}
 }
