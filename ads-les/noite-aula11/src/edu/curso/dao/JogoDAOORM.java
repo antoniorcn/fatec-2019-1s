e1bbec235dae519b396b433c197f54087c000452
Antonio Rodrigues <antonio@moonsoft.com.br>
2019-04-24 21:37:12 -0300

    LES Noite Aula 11 parte 2

--- ads-les/noite-aula11/src/edu/curso/dao/JogoDAOORM.java
+++ ads-les/noite-aula11/src/edu/curso/dao/JogoDAOORM.java
@@ -1,8 +1,8 @@
 package edu.curso.dao;
-import java.sql.Connection;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import edu.curso.Jogo;
 public class JogoDAOORM implements JogoDAO {
@@ -17,31 +17,45 @@ public class JogoDAOORM implements JogoDAO {
 
 	@Override
 	public void atualizar(long id, Jogo g) throws DAOException {
-		// TODO Auto-generated method stub
-		
+		EntityManager em = ConnectionBuilderORM.getInstance().getConnection();
+		em.getTransaction().begin();
+		em.merge(g);
+		em.getTransaction().commit();
+		em.close();
 	}
-
 	@Override
 	public void remover(long id) throws DAOException {
-		// TODO Auto-generated method stub
-		
+		EntityManager em = ConnectionBuilderORM.getInstance().getConnection();
+		Jogo j = em.find(Jogo.class, id);
+		em.getTransaction().begin();
+		em.remove(j);
+		em.getTransaction().commit();
+		em.close();
 	}
-
 	@Override
 	public List<Jogo> pesquisarPorNome(String nome) throws DAOException {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = ConnectionBuilderORM.getInstance().getConnection();
+		TypedQuery<Jogo> qry = em.createQuery(
+				"select j from Jogo j where nome like :nome", Jogo.class);
+		qry.setParameter("nome", "%" + nome + "%");
+		List<Jogo> lista = qry.getResultList();
+		em.close();
+		return lista;
 	}
-
 	@Override
 	public List<Jogo> pesquisarTodos() throws DAOException {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = ConnectionBuilderORM.getInstance().getConnection();
+		TypedQuery<Jogo> qry = em.createQuery("select j from Jogo j", Jogo.class);
+		List<Jogo> lista = qry.getResultList();
+		em.close();
+		return lista;
 	}
 
 	@Override
 	public Jogo pesquisarPorId(long id) throws DAOException {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = ConnectionBuilderORM.getInstance().getConnection();
+		Jogo j = em.find(Jogo.class, id);
+		em.close();
+		return j;
 	}
 }
