770a4edfba5a6b36419fe6a25b7635ba5b035ce0
Antonio Rodrigues Carvalho <antoniorcn@hotmail.com>
2019-05-15 21:57:09 -0300

    No lab 102

---
+++ ads-les/teste-spring-6/src/edu/curso/AgendaController.java
@@ -0,0 +1,39 @@
+package edu.curso;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import edu.curso.entidade.Pessoa;	
+
+@Controller
+public class AgendaController {
+	@RequestMapping("/agenda")
+	public ModelAndView agenda() { 
+		Pessoa pessoa = new Pessoa();
+		pessoa.setEmail("...@...");
+		return new ModelAndView("agenda", "pessoa", pessoa);
+	}
+	@RequestMapping("/agendaCommand")
+	public ModelAndView agendaCommand(@RequestParam String cmd,  @ModelAttribute("pessoa") Pessoa pessoa) {
+		System.out.println("Pessoa recebida como atributo: " + pessoa);
+		if (pessoa == null) { 
+			pessoa = new Pessoa();
+		}
+		if ("adicionar".equals(cmd)) { 
+			System.out.println(
+					String.format("Pessoa: %s adicionada ao database", pessoa.getNome()));
+		} else if ("pesquisar".equals(cmd)) {
+			System.out.println(
+					String.format("Procurando pessoa: %s no banco de dados", pessoa.getNome()));
+			pessoa = new Pessoa();
+			pessoa.setNome("Joao 1");
+			pessoa.setTelefone("1234567");
+			pessoa.setEmail("joao@teste.com");
+		}
+		ModelAndView model = new ModelAndView("agenda", "pessoa", pessoa);
+		return model; 
+	}
+}
---
+++ ads-les/teste-spring-6/src/edu/curso/entidade/Pessoa.java
@@ -0,0 +1,41 @@
+package edu.curso.entidade;
+
+public class Pessoa {
+	private long id;
+	private String nome;
+	private String telefone;
+	private String email;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Override
+	public String toString() { 
+		return getNome() + " - " + getTelefone() + " - " + getEmail();
+	}
+}
