63e66abcadab0a24ebb2dcb4cb24856217c63a9a
Antonio Rodrigues <antonio@moonsoft.com.br>
2019-05-17 17:35:58 -0300

    Aula 14 poo tarde parte 3

--- ads-poo/tarde-aula14/src/edu/curso/control/PersonagemControl.java
+++ ads-poo/tarde-aula14/src/edu/curso/control/PersonagemControl.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
@@ -73,10 +74,34 @@ public class PersonagemControl {
 	
 	public void pesquisar(String nome) { 
 		tableData.clear();
-		for (Personagem p : lista) { 
-			if(p.getNome().contains(nome)) { 
+//		for (Personagem p : lista) { 
+//			if(p.getNome().contains(nome)) { 
+//				tableData.add(p);
+//			}
+//		}
+		try {
+			Connection con = 
+					DriverManager.getConnection(url, user, password);
+			String sql = "SELECT * FROM personagem WHERE nome like ?";
+			// System.out.println("Query de pesquisa: " + sql);
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setString(1, "%" + nome + "%");
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) { 
+				Personagem p = new Personagem();
+				p.setId(rs.getLong("id"));
+				p.setNome(rs.getString("nome"));
+				p.setHabilidade(rs.getString("habilidade"));
+				p.setAltura(rs.getFloat("altura"));
+				p.setForca(rs.getFloat("forca"));
+				p.setDoMal(rs.getBoolean("do_mal"));
+				p.setNascimento(rs.getDate("nascimento"));
 				tableData.add(p);
+				// System.out.println(rs.getString("nome") + " - " + rs.getString("habilidade"));
 			}
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 	}
 
