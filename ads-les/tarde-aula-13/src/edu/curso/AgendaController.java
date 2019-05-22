30b2662db5b2617da5d6a492d4795537d822bd81
Antonio Rodrigues <antonio@moonsoft.com.br>
2019-05-22 17:43:12 -0300

    LES Tarde Aula 13 parte 4

--- ads-les/tarde-aula-13/src/edu/curso/AgendaController.java
+++ ads-les/tarde-aula-13/src/edu/curso/AgendaController.java
@@ -1,7 +1,9 @@
 package edu.curso;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -16,6 +18,19 @@ public class AgendaController {
 	public ModelAndView agenda() { 
 		Contato c = new Contato();
 		c.setEmail("...@...");
+		ModelAndView mv = new ModelAndView("agenda", "contato", c);
+		return mv;
+	}
+	
+	@RequestMapping("/agendaProcessa")
+	public ModelAndView agendaProcessa(
+			@ModelAttribute("contato") Contato c,
+			@RequestParam("cmd") String cmd) {
+		System.out.println("Botao Apertado: " + cmd);
+		System.out.println("Nome: " + c.getNome());
+		System.out.println("Email: " + c.getEmail());
+		System.out.println("Telefone: " + c.getTelefone());
+		c = new Contato();
 		ModelAndView mv = new ModelAndView("agenda", "contato", c);
 		return mv;
 	}
