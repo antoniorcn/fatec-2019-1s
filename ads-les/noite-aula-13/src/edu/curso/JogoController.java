5966cf0b16951aac14d84dcfae8f5e28b3cc203f
Antonio Rodrigues <antonio@moonsoft.com.br>
2019-05-22 22:04:19 -0300

    LES Noite Aula 13 parte 3

--- ads-les/noite-aula-13/src/edu/curso/JogoController.java
+++ ads-les/noite-aula-13/src/edu/curso/JogoController.java
@@ -1,15 +1,33 @@
 package edu.curso;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class JogoController {
 
 	@RequestMapping("/jogo")
-	public String jogo() { 
-		System.out.println("Controller Jogo executado");
-		return "jogo";
+	public ModelAndView jogo() {
+		Jogo j = new Jogo();
+		j.setNome("Pacman");
+		ModelAndView mv = new ModelAndView("jogo", "jogo", j);
+		System.out.println("Jogo foi executado");
+		return mv;
 	}
 		
+	@RequestMapping("/jogoController")
+	public ModelAndView jogoController(
+			@ModelAttribute("jogo") Jogo j,
+			@RequestParam("cmd") String cmd) {
+		System.out.println("Jogo Controller foi executado");
+		System.out.println("Foi apertado o botão: " + cmd);
+		System.out.println("Nome do Jogo: " + j.getNome());
+		System.out.println("Tipo do Jogo: " + j.getTipo());
+		System.out.println("\n");
+		ModelAndView mv = new ModelAndView("jogo", "jogo", j);
+		return mv;
+	}
 }
