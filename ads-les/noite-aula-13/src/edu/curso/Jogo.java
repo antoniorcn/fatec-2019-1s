c088d2ef1abe304290df2da3997f99727ee65d71
Antonio Rodrigues <antonio@moonsoft.com.br>
2019-05-22 22:04:26 -0300

    LES Noite Aula 13 parte 3

---
+++ ads-les/noite-aula-13/src/edu/curso/Jogo.java
@@ -0,0 +1,28 @@
+package edu.curso;
+
+public class Jogo {
+	private long id;
+	private String nome;
+	private String tipo;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+}
