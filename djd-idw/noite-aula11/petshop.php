24c3b0fd65ecaa6ec5d37777ea1f740d1b3984b9
Antonio Rodrigues <antonio@moonsoft.com.br>
2019-05-23 22:21:29 -0300

    Fatec IDW noite aula 12 parte 2

--- djd-idw/noite-aula11/petshop.php
+++ djd-idw/noite-aula11/petshop.php
@@ -2,6 +2,15 @@
 	<body>
 		<h1>Pet Shop</h1>
 		<h2>Gestão de Animais</h2>
+		<?php
+			session_start();
+			if (isset($_SESSION['MENSAGEM'])) {
+		?>
+				<h3><?=$_SESSION['MENSAGEM']?></h3>
+		<?php
+				unset($_SESSION['MENSAGEM']);
+			}
+		?>			
 		<form action="./petshop_controller.php"
 			  method="post">
 		<table>
@@ -22,10 +31,24 @@
 				<td><input type="text" name="PESO"></td>
 			</tr>
 			<tr>
-				<td><button type="submit" value="adicionar">Adicionar</button></td>
-				<td><button type="submit" value="pesquisar">Pesquisar</button></td>
+				<td><button type="submit" name="CMD" value="adicionar">Adicionar</button></td>
+				<td><button type="submit" name="CMD" value="pesquisar">Pesquisar</button></td>
 			</tr>					
 		</table>
 		</form>
+		<?php
+			if (isset($_SESSION['LISTA'])) {
+				$lista = $_SESSION['LISTA'];
+				foreach($lista as $registro) {
+		?>
+					<h3><?=$registro['nome']?></h3>
+					<h3><?=$registro['nascimnto']?></h3>
+					<h3><?=$registro['peso']?></h3>
+		<?php
+				}
+			}
+		?>	
+		
+		
 	</body>
 </html>
--- djd-idw/noite-aula11/petshop_controller.php
+++ djd-idw/noite-aula11/petshop_controller.php
@@ -1,16 +1,32 @@
 <?php
 
+	session_start();
+
 	$nome = $_REQUEST['NOME'];
 	$nasc = $_REQUEST['NASCIMENTO'];
 	$peso = $_REQUEST['PESO'];
+	$cmd = $_REQUEST['CMD'];
 	
+
 	$db = new PDO(
 	'mysql:host=localhost;dbname=petshop;charset=utf8', 
 	'root', '');
 	print "Conexão OK! <BR>"; 
-	//		$filme = $_REQUEST['FILME'];
-	//
-	//		$sql = "INSERT INTO filme VALUES ('" . $filme . "')";
-	//		$db->exec($sql);
 
+	if ($cmd == "adicionar") { 
+		$sql = "INSERT INTO animais VALUES ('$nome', '$nasc', $peso);";
+		$db->exec($sql);
+		$_SESSION['MENSAGEM'] = "Animal foi inserido com sucesso";
+		header('Location: ./petshop.php');
+	} else if ($cmd == "pesquisar") { 
+		$sql = "SELECT * FROM animais WHERE nome = '$nome'";
+		print "Pesquisando animal";
+		$stmt = $db->query($sql);
+		$lista = array();
+		forEach( $stmt as $registro ) { 
+			array_push($lista, $registro);
+		}
+		$_SESSION['LISTA'] = $lista;
+		header('Location: ./petshop.php');
+	}
 ?>
