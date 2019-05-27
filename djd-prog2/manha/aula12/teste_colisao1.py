254ed6fb519cc7b56b5de203bc69df95fa3dd624
Antonio Rodrigues <antonio@moonsoft.com.br>
2019-05-27 10:11:57 -0300

    PROG2 Manha aula 12 parte 2

--- djd-prog2/manha/aula12/teste_colisao1.py
+++ djd-prog2/manha/aula12/teste_colisao1.py
@@ -6,14 +6,22 @@ screen = pygame.display.set_mode((800, 600), 0, 32)
 class Personagem(pygame.sprite.Sprite):
     def __init__(self):
         self.rect = Rect((100, 100), (50, 50))
+        self.velocidade = [0, 0]
 
     def draw(self, tela):
         pygame.draw.rect(tela, (255, 255, 0), self.rect, 0)
+
+    def update(self, *args):
+        super().update(*args)
+        nova_pos = (self.rect.topleft[0] + self.velocidade[0],
+                    self.rect.topleft[1] + self.velocidade[1])
+        self.rect.topleft = nova_pos
 
 
 heroi = Personagem()
 while True:
     # Calcular regras
+    heroi.update()
 
     # Pintar
     screen.fill((0, 0, 0))
@@ -24,3 +32,6 @@ while True:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             exit()
+        if e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_SPACE:
+               heroi.velocidade = [1, 0]
