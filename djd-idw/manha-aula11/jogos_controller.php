376f79623b0b297ae9830cc465dd5e530b43c246
Antonio Rodrigues <antonio@moonsoft.com.br>
2019-05-27 09:20:27 -0300

    IDW Manha aula 11 parte 4

--- djd-idw/manha-aula11/jogos_controller.php
+++ djd-idw/manha-aula11/jogos_controller.php
@@ -10,8 +10,15 @@
 	);
 	echo "Conexão OK! <BR>";
 	$sql = "INSERT INTO jogos VALUES ( " .
-		"$id, '$nome', '$genero', $jogou, $rec, '$imagem')";
-	$db->exec($sql);
+		"$id, :nome, :genero, :jogou, :rec, :imagem)";
+	$stmt = $db->prepare($sql);
+	$stmt->bindValue(':nome', $nome, PDO::PARAM_STR);
+	$stmt->bindValue(':genero', $genero, PDO::PARAM_STR);
+	$stmt->bindValue(':jogou', $jogou, PDO::PARAM_INT);
+	$stmt->bindValue(':rec', $rec, PDO::PARAM_INT);
+	$stmt->bindValue(':imagem', $imagem, PDO::PARAM_STR);
+	$stmt->execute();
+	$result = $stmt->rowCount();
 	echo "Comando executado com sucesso <BR>";
 	header('Location: ./jogos.php');
 	$msg = "Jogo cadastrado com sucesso";
