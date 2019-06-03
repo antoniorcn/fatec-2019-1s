c207ed9d1136413c883b23410883081775877341
Antonio Rodrigues <antonio@moonsoft.com.br>
2019-06-03 10:28:29 -0300

    Aula 13 PROG2 manha parte 2

--- djd-prog2/manha/aula13/teste_sprites.py
+++ djd-prog2/manha/aula13/teste_sprites.py
@@ -3,21 +3,23 @@ import pygame
 screen = pygame.display.set_mode((800, 600), 0, 32)
 
 
-class Quadro():
-    def __init__(self):
-        self.x = 10
-        self.y = 10
+class Quadro(pygame.sprite.Sprite):
+    def __init__(self, pos, cor):
+        super().__init__()
+        self.rect = pygame.Rect(pos, (50, 50))
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(cor)
         self.vel_x = 0
         self.vel_y = 0
 
-    def calcular(self):
-        self.x = self.x + self.vel_x
-        self.y = self.y + self.vel_y
+    def update(self):
+        self.rect.x = self.rect.x + self.vel_x
+        self.rect.y = self.rect.y + self.vel_y
 
-    def pintar(self, tela):
-        pygame.draw.rect(tela,
-                         (255, 0, 0),
-                         ((self.x, self.y), (100, 100)), 0)
+    # def pintar(self, tela):
+    #     pygame.draw.rect(tela,
+    #                      (255, 0, 0),
+    #                      self.rect, 0)
 
     def testar_eventos(self, e):
         if e.type == pygame.KEYDOWN:
@@ -30,25 +32,22 @@ class Quadro():
             elif e.key == pygame.K_DOWN:
                 self.vel_y = 1
 
-q1 = Quadro()
-q2 = Quadro()
-q3 = Quadro()
 
-q2.x = 100
+q1 = Quadro((10, 10), (255, 0, 0))
+q2 = Quadro((200, 50), (0, 0, 255))
+q3 = Quadro((400, 100), (255, 255, 0))
 
-q3.x = 300
+grp = pygame.sprite.Group()
+
+grp.add([q1, q2, q3])
 
 while True:
     # Calcular Regras
-    q1.calcular()
-    q2.calcular()
-    q3.calcular()
+    grp.update()
 
     # Pinta na tela
     screen.fill((0, 0, 0))
-    q1.pintar(screen)
-    q2.pintar(screen)
-    q3.pintar(screen)
+    grp.draw(screen)
     pygame.display.update()
 
     # Testa os eventos
@@ -56,6 +55,5 @@ while True:
         if e.type == pygame.QUIT:
             exit()
         else:
-            q1.testar_eventos(e)
-            q2.testar_eventos(e)
-            q3.testar_eventos(e)
+            for spr in grp.sprites():
+                spr.testar_eventos(e)
