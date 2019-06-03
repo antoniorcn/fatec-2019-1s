fc204ce0658683b2c560538806b52eb99a03303d
Antonio Rodrigues <antonio@moonsoft.com.br>
2019-06-03 10:50:58 -0300

    Aula 13 PROG2 manha parte 4

--- djd-prog2/manha/aula13/teste_sprites.py
+++ djd-prog2/manha/aula13/teste_sprites.py
@@ -3,6 +3,21 @@ import pygame
 screen = pygame.display.set_mode((800, 600), 0, 32)
 
 
+class Tiro(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        self.rect = pygame.Rect(pos, (5, 5))
+        self.image = pygame.Surface((5, 5))
+        self.image.fill((255, 255, 0))
+        self.vel_x = 1
+
+    def update(self):
+        if self.rect.x + self.vel_x < 500:
+            self.rect.x = self.rect.x + self.vel_x
+        else:
+            self.kill()
+
+
 class Quadro(pygame.sprite.Sprite):
     def __init__(self, pos, cor, teclas):
         super().__init__()
@@ -12,15 +27,12 @@ class Quadro(pygame.sprite.Sprite):
         self.teclas = teclas
         self.vel_x = 0
         self.vel_y = 0
+        self.tiros = pygame.sprite.Group()
 
     def update(self):
         self.rect.x = self.rect.x + self.vel_x
         self.rect.y = self.rect.y + self.vel_y
-
-    # def pintar(self, tela):
-    #     pygame.draw.rect(tela,
-    #                      (255, 0, 0),
-    #                      self.rect, 0)
+        self.tiros.update()
 
     def testar_eventos(self, e):
         if e.type == pygame.KEYDOWN:
@@ -32,12 +44,15 @@ class Quadro(pygame.sprite.Sprite):
                 self.vel_y = -1
             elif e.key == self.teclas[3]:
                 self.vel_y = 1
+            elif e.key == self.teclas[4]:
+                t = Tiro(self.rect.midright)
+                self.tiros.add(t)
 
 
 q1 = Quadro((10, 10), (255, 0, 0), [pygame.K_RIGHT, pygame.K_LEFT,
-                                    pygame.K_UP, pygame.K_DOWN])
+                                    pygame.K_UP, pygame.K_DOWN, pygame.K_SPACE])
 q2 = Quadro((200, 50), (0, 0, 255), [pygame.K_d, pygame.K_a,
-                                    pygame.K_w, pygame.K_s])
+                                    pygame.K_w, pygame.K_s, pygame.K_c])
 # q3 = Quadro((400, 100), (255, 255, 0))
 
 grp = pygame.sprite.Group()
@@ -51,6 +66,8 @@ while True:
     # Pinta na tela
     screen.fill((0, 0, 0))
     grp.draw(screen)
+    q1.tiros.draw(screen)
+    q2.tiros.draw(screen)
     pygame.display.update()
 
     # Testa os eventos
